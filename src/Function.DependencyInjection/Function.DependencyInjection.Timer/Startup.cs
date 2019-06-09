6269b4989d5ef4d6c6da5e3f2561a7360dc9a57c
Kasun Kodagoda <kvkrusl@gmail.com>
2019-06-09 14:15:13 +0530

    Installed the NuGet packages and Startup class

---
+++ src/Function.DependencyInjection/Function.DependencyInjection.Timer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Function.DependencyInjection.Services;
+using Function.DependencyInjection.Services.Contracts;
+using Function.DependencyInjection.Timer;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Function.DependencyInjection.Timer
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<IHelloService, HelloService>();
+        }
+    }
+}
